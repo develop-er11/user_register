2860ef7da9faf0d54425741469764e7d3a14ee17
Rutuja Deshmukh <66289022+develop-er11@users.noreply.github.com>
2022-12-24 18:50:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,37 +3,22 @@ const dotenv = require('dotenv')
 const connectDB = require("./config/db")
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const userRoutes=require('./routes/userRoutes')
+const path = require('path')
 
-const app = express();
+
 dotenv.config();
+
 connectDB();
+
+const app = express(); // main thing
+
 app.use(express.json()); // to accept json data
-
-
-app.get('/', (req, res) => {
-    res.send("API is running..");
-});
 
 
 app.use("/api/users", userRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`server started on PORT ${PORT}`)); 
-
-
-
-
-
-//import path from "path";
-
-
-
 // --------------------------deployment------------------------------
-/*const __dirname = path.resolve();
+__dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
@@ -46,4 +31,20 @@ if (process.env.NODE_ENV === "production") {
     res.send("API is running..");
   });
 }
-*/
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`
+  )
+);
+
+
+
